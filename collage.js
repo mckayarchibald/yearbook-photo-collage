2e955aa3cb4ca0697c8996815e86718a1f2857b4
McKay Archibald <mck.archibald@gmail.com>
2021-04-19 22:09:17 -0600

    finished basica collage javascript

---
+++ collage.js
@@ -0,0 +1,51 @@
+const canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth * 2;
+canvas.height = window.innerHeight * 2;
+canvas.style.width = window.innerWidth + "px";
+canvas.style.height = window.innerHeight + "px";
+
+const context = canvas.getContext("2d");
+context.scale(2, 2);
+
+let aimX = null;
+let aimY = null;
+let currentX = null;
+let currentY = null;
+
+let index = 0;
+const images = ["mickey.png", "mickey2.png"].map(src => {
+    const image = document.createElement("img");
+    image.src = src;
+    return image;
+})
+
+document.addEventListener("mousemove", function(event) {
+    aimX = event.pageX;
+    aimY = event.pageY;
+    if(currentX === null) {
+        currentX = event.pageX;
+        currentY = event.pageY;
+    }
+})
+
+canvas.addEventListener("click", function() {
+    index++;
+    if(index >= images.length) {
+        index = 0;
+    }
+})
+
+const draw = function() {
+    if(currentX) {
+        if(images[index].complete) {
+            context.drawImage(images[index], currentX - 200, currentY - 300, 600, 400) ;
+        }
+
+        currentX = currentX + (aimX - currentX) * 0.05;
+        currentY = currentY + (aimY - currentY) * 0.05;
+    }
+
+    requestAnimationFrame(draw);
+}
+
+draw();
